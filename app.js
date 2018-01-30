0ab3d5df97439deb3e24c30dcb242f5c744dfb9a
Steele Walston <steele.walston@gmail.com>
2018-01-30 15:11:37 -0800

    Added all questions and debugged

--- app.js
+++ app.js
@@ -6,17 +6,69 @@ console.log('I am Javascript');
             console.log('name is a ' + typeof name);
             console.log('Their name is: ' + name);
 
-            const golf = prompt ('Do I love golf?').toUpperCase();
-            console.log('They said ' + golf);
-            if (golf === 'yes' || 'y') {
-                alert ('Yes! I love golf, even though it is extremely difficult');
-            } else if (golf === 'no', 'n') {
-                alert ('Nope! I\'m super big on golf. I have multiple golf shirts');
+            const golf = prompt ('Do I love golf?').toLowerCase();
+            if (golf === 'yes' || golf === 'y') {
+                alert ('You\'re right! I love golf, even though it is extremely difficult');
+            } else if (golf === 'no' || golf === 'n') {
+                alert ('Nope! I\'m super big on golf. I even have multiple golf shirts');
             } else {
-                alert('You didn\'t pick one!');
+                alert('You didn\'t wirte a valid answer!');
             }
+            console.log('Do I love golf?: They answered ' + golf);
+
+
+            const hike = prompt ('Am I the kind of person who enjoys a good hike?').toLowerCase();
+            if (hike === 'yes' || hike === 'y') {
+                alert ('You\'re right! My favorite hike so far this year has been Labyrnth Falls');
+            } else if (hike === 'no' || hike === 'n') {
+                alert ('Wrong! I quite enjoy hiking, especially when the landscape reminds me of Lord of the Rings');
+            } else {
+                alert('You didn\'t wirte a valid answer!');
+            }
+            console.log('Do I like to hike?: They answered ' + hike);
+
+
+            const peanuts = prompt ('Would I eat a peanut butter and jelly sandwich?').toLowerCase();
+            if (peanuts === 'yes' || peanuts === 'y') {
+                alert ('You\'re wrong! Why would I do such a thing? I\'m allergic to peanuts remember?!');
+            } else if (peanuts === 'no' || peanuts === 'n') {
+                alert ('Your\'re right! I\'m extremly alleric to peanuts and I might die if I ate such a sandwich');
+            } else {
+                alert('You didn\'t wirte a valid answer!');
+            }
+            console.log('Would I eat penuts?: They answered ' + peanuts);
+
+
+            const coding = prompt ('Do I love writing code?').toLowerCase();
+            if (coding === 'yes' || coding === 'y') {
+                alert ('You\'re right! Well so far. I\'m pretty new to it but I\'m really enjoying the new challenges');
+            } else if (coding === 'no' || coding === 'n') {
+                alert ('Wrong! I\'m having a really good time writing code');
+            } else {
+                alert('You didn\'t wirte an valid answer!');
+            }
+            console.log('Do I love writing code?: They answered ' + coding);
+
+            const broken = prompt ('Have I ever broken a bone?').toLowerCase();
+            if (broken === 'yes' || broken === 'y') {
+                alert ('You\'re wrong! Well so far. I\'ve had some close calls though');
+            } else if (broken === 'no' || broken === 'n') {
+                alert ('Yep! All bones fully intact!');
+            } else {
+                alert('You didn\'t wirte an valid answer!');
+            }
+            console.log('Have I broken a bone?: They answered ' + broken);
+
+            const harryPotter = prompt ('Bonus! You must know me very well if you can respond correctly to this question: You\'re a wizard Harry.').toLowerCase();
+            if (harryPotter === 'i\'m a what?') {
+                alert ('A wizard');
+            } else {
+                alert('You didn\'t write the correct response');
+            }
+            console.log('Harry Potter secret test: They answered ' + harryPotter);
             
             // Previous questions that are Non Yes/No 
+
             /* Asking the user where they are from
             const hometown = prompt('So, ' + name + ' now that we\'ve formally introduced ourselves, Where are you from?');
             alert('Oh cool! I\'ve never been to ' + hometown + '! I\'m from Conifer, Colorado, the sunniest sate in the US!');
